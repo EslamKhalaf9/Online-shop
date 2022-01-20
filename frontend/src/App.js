66205268b2025ead970ec45c690b54708ecab967
EslamKhalaf9 <eslamkhalaf191@gmail.com>
2022-01-20 17:57:58 +0200

    react-setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <h1>Eslam Khalaf</h1>
+      </Container>
+    </>
+  );
+};
+
+export default App;
