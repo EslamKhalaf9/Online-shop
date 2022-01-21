858f5736f0ba5de2327dc9faa7535750d92be0da
EslamKhalaf9 <eslamkhalaf191@gmail.com>
2022-01-21 16:24:10 +0200

    adding react router links

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <h1 className='mt-2 text-center'>Products List</h1>
-      <Row className='mb-2'>
+      <Row className='mb-2 -webkit-center'>
         {products.map((product) => (
           <Product key={product._id} product={product} />
         ))}
--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { Col, Card, Row, ListGroup, Button, Image } from "react-bootstrap";
 
 import products from "../products";
+import Rate from "../components/Rate";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -11,10 +13,64 @@ const ProductDetails = () => {
     setProduct(myProduct);
   }, []);
   return (
-    <div>
-      hey i'm product and to prove that here is my title {product.name} do you
-      believe me now ?!
-    </div>
+    <>
+      <Link className='btn btn-dark my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item className='bg-dark'>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item className='bg-dark'>
+              <Rate rating={product.rating} numReviews={product.numReviews} />
+            </ListGroup.Item>
+            <ListGroup.Item className='bg-dark'>
+              Price: ${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item className='bg-dark'>
+              Description: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item className='bg-dark'>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item className='bg-dark'>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className='bg-dark'>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
